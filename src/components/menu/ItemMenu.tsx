c27f3adde67dcad482daec6ae2925b09045a1665
Carel Ntsoumou Lihoula <carellihoula10@gmail.com>
2024-10-01 17:25:45 +0200

    feat: added tooltip to menu items for better UX

--- src/components/menu/ItemMenu.tsx
+++ src/components/menu/ItemMenu.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IMenuItem } from "../../types__interfaces/interface";
 import styled from "styled-components";
+import { Tooltip } from "react-tooltip";
 
 interface MenuItemProps {
   item: IMenuItem;
@@ -8,16 +9,23 @@ interface MenuItemProps {
 
 const MenuItem: React.FC<MenuItemProps> = ({ item }) => {
   return (
-    <MenuStyled className="menu-item">
-      {item.icon && <item.icon size={35} className="icon" />}
-      {item.imgUrl && (
-        <div className="avatar">
-          <div className="mask mask-squircle w-16">
-            <img src={item.imgUrl} alt={item.title} className="menu-img" />
+    <>
+      <MenuStyled
+        className="menu-item"
+        data-tooltip-id={`tooltip-${item.id}`}
+        data-tooltip-content={item.title}
+      >
+        {item.icon && <item.icon size={35} className="icon" />}
+        {item.imgUrl && (
+          <div className="avatar">
+            <div className="mask mask-squircle w-16">
+              <img src={item.imgUrl} alt={item.title} className="menu-img" />
+            </div>
           </div>
-        </div>
-      )}
-    </MenuStyled>
+        )}
+      </MenuStyled>
+      <Tooltip id={`tooltip-${item.id}`} place="left" style={{ zIndex: 2 }} />
+    </>
   );
 };
 
