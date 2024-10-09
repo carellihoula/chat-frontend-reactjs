a0d194b5ea2bb359e6ecaaa67c17e0628092371a
Carel Ntsoumou Lihoula <carellihoula10@gmail.com>
2024-10-09 23:55:43 +0200

    fix: auto-scroll on new message

--- src/section/RightSide.tsx
+++ src/section/RightSide.tsx
@@ -1,14 +1,37 @@
 import styled from "styled-components";
 import MessageInput from "../components/message/MessageInput";
 import SingleMessage from "../components/message/SingleMessage";
-import { messages } from "../mock/messages";
+import { messages as initialMessages } from "../mock/messages";
 import ChatHeader from "../components/ChatHeader";
 import { users } from "../mock/users";
+import { useEffect, useRef, useState } from "react";
+import { Message } from "../types__interfaces/interface";
 
 const RightSide = () => {
-  const handleSendMessage = (message: string) => {
-    console.log("Message sent:", message);
+  const [messages, setMessages] = useState(initialMessages);
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
+
+  const handleSendMessage = (messageContent: string) => {
+    console.log("Message sent:", messageContent);
+    const newMessage: Message = {
+      id: messages.length + 1,
+      sender: "Vous",
+      receiver: "Alice",
+      content: messageContent,
+      timestamp: new Date(),
+      senderPhoto: "https://randomuser.me/api/portraits/men/1.jpg",
+    };
+    setMessages([...messages, newMessage]);
   };
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "auto" });
+  };
+  //console.log(messages);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
   return (
     <RightSideStyled>
       <ChatHeader person={users[0]} />
@@ -16,6 +39,7 @@ const RightSide = () => {
         {messages.map((message) => (
           <SingleMessage key={message.id} message={message} />
         ))}
+        <div ref={messagesEndRef} />
       </Messages>
 
       <MessageInput onSendMessage={handleSendMessage} />
